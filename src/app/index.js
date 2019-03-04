9c7e5e973990c387691691d38cde3aa452c27aa8
daliuspamedytis <daliuspamedytis1393@gmail.com>
2019-03-04 22:09:45 +0200

    added favourite button

--- src/app/index.js
+++ src/app/index.js
@@ -23,8 +23,23 @@ class App extends React.Component {
     this.setState({ loading: true });
     fetch("https://boiling-reaches-93648.herokuapp.com/food-shop/products")
       .then(response => response.json())
-      .then(json => this.setState({ products: json, loading: false }))
+      .then(json => {
+        const products = json.map(product => ({...product, isFavorite: false}));
+        this.setState({ products, loading: false });
+      })
       .catch(() => this.setState({ error: "Something went wrong", loading: false }));
+  }
+
+  toggleFavourite = id => {
+    this.setState(state =>({
+      products: state.products.map(product =>{
+        if (product.id === id) {
+          return {...product, isFavorite: !product.isFavorite}
+
+        }
+        return product
+      })
+    }))
   }
 
   render() {
@@ -34,7 +49,7 @@ class App extends React.Component {
         {error && <span>{error}</span>}
         {loading && <PacmanLoader/>}
 
-        <Shop products={products} />
+        <Shop products={products} toggleFavourite={this.toggleFavourite} />
       </PageLayout>
     );
   }
--- src/app/pages/Shop.js
+++ src/app/pages/Shop.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 var FontAwesome = require('react-fontawesome');
 
-function Shop({products}) {
+function Shop({products, toggleFavourite}) {
 
   return (
     <div className='mainCard'>
@@ -10,9 +10,9 @@ function Shop({products}) {
         <div className="card">
           <img src={product.image} alt={product.name}></img>
           <p className="name" key={product.id}>{product.name}</p>
-          <p className="price">{product.price} {product.currency}</p>
+          <p className="price"><b>price: </b>{product.price} {product.currency}</p>
           <button type='button'><span role='img' aria-label=''>🛒</span></button>
-          <button type='button'><span role='img' aria-label=''>⭐</span> </button>
+          <button onClick={() => toggleFavourite(product.id)}type='button'><span role='img' aria-label=''>{product.isFavorite ? "💚" : "⭐"}</span> </button>
         </div>
       ))}
     </div>
